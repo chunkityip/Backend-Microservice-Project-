7915032608702d3946ec8ef3e57ed53f6f428350
chunkityip <alex0917lfo@hotmail.com>
2023-07-24 10:38:27 -0400

    Circuit Breaker

--- order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
@@ -2,9 +2,14 @@ package com.programmingtechie.orderservice.controller;
 
 import com.programmingtechie.orderservice.dto.OrderRequest;
 import com.programmingtechie.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -15,8 +20,16 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order Placed Successfully";
+    @CircuitBreaker(name = "inventory" , fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+    }
+
+    //A method for display error message to ensure the client to wait
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() -> "We apologize for the inconvenience. Our team is currently working on the issue, and we kindly ask for your patience. " +
+                "We will resolve it as soon as possible. Thank you for your understanding.");
     }
 }
